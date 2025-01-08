58fcfe77c56d7014610af6a616903f5b6b8d6d7f
jake baek <jake.baek@gmail.com>
2025-01-08 23:47:43 +0900

    #1 - PUT /api/users/me API 구현

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse, UserUpdate
 from app.core.utils import get_current_user
+from app.core.database import get_db
+from app.models.user import User
 
 router = APIRouter(prefix="/api/users", tags=["users"])
 
@@ -11,6 +13,28 @@ def get_me(current_user: UserResponse = Depends(get_current_user)):
     return current_user
 
 
-@router.put("/me")
-def update_me():
-    return {"message": "update me"}
+@router.put(
+    "/me",
+    response_model=UserResponse,
+    responses={
+        404: {
+            "description": "User not found",
+            "content": {"application/json": {"example": {"detail": "User not found"}}},
+        }
+    },
+)
+def update_me(
+    user_update: UserUpdate,
+    db: Session = Depends(get_db),
+    current_user: UserResponse = Depends(get_current_user),
+):
+    db_user = db.query(User).filter(User.id == current_user.id).first()
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+
+    db.user.nickname = user_update.nickname
+    db.commit()
+    db.refresh(db_user)
+    return db_user
