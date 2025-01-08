cafc35f47f4024fd4728e2719f409e135ae87797
jake baek <jake.baek@gmail.com>
2025-01-08 20:58:40 +0900

    #1 - DB 초기화 코드 추가

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -9,6 +9,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
 def get_db():
     db = SessionLocal()
     try:
